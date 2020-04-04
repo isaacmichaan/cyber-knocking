19452041c98ff2aa8d77c2296faae34e385d8280
Isaac Michaan <46087877+isaacmichaan@users.noreply.github.com>
2020-04-04 22:16:33 +0300

    Update knocking.py

--- knocking.py
+++ knocking.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 import os
 
+key = "isaacmic"
 knocking = []
 ok = [[8080, "RA"], [8834, "SA"], [8840, "R"]]
 
@@ -10,7 +11,10 @@ def handler(pkt):
 	if len(knocking) > 3:
 		del(knocking[0])
 	print(knocking)
-	if knocking == ok:
-		os.system(f"iptables -I INPUT 1 -p tcp -s {pkt['IP'].src} --dport 22 -j ACCEPT")
+	try:
+		if sxor(key, pkt['Raw'].load.decode()) and knocking == ok:
+			os.system(f"iptables -I INPUT 1 -p tcp -s {pkt['IP'].src} --dport 22 -j ACCEPT")
+	except IndexError:
+		pass
 
 sniff(filter="tcp and portrange 8000-9000", prn=handler)
